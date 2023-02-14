17b01a17481b3c3c9bba12a44803b86dd4c50fdc
JoaoPaulo-Web <joaopaulocoimbra2013@gmail.com>
2023-02-14 12:44:24 -0300

    Todos

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import React, {useState} from "react";
+import TodoList from "./TodoList";
 
 function App() {
-  return null
+  const [todos, setTodos] = useState([])
+  return (
+    <>
+    <TodoList />
+    <input type="text"/>
+    <button>Add Todo</button>
+    <button>Clear Completed Todos</button>
+    <div>0 left to do</div>
+    </>
+  )
 }
-
+ 
 export default App;
